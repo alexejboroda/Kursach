0401fe01248cd20f38d94fcb228341c0ee318a9c
alexejboroda <65348795+alexejboroda@users.noreply.github.com>
2020-05-14 17:25:45 +0300

    Add files via upload

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+    protected
+    def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) {|u|  u.permit(:username,:email,:password,:password_confirmation,:roll) }
+       devise_parameter_sanitizer.permit(:sign_in) {|u|  u.permit(:username,:email,:password,:password_confirmation,:roll) }
+    
+    end
+end
